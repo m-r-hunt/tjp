88a1febf06a0c8c86c331eb3278da8ae62dffe56
Max Hunt <max@huntw3.com>
2019-10-03 08:57:23 +0100

    Not working

--- src/main.zig
+++ src/main.zig
@@ -2,39 +2,77 @@ const std = @import("std");
 const testing = std.testing;
 const json = std.json;
 const ArrayList = std.ArrayList;
+const math = std.math;
+const TypeInfo = @import("builtin").TypeInfo;
 
-export fn add(a: i32, b: i32) i32 {
-    return a + b;
+fn i64InIntegerRange(comptime T: type, value: i64) bool {
+    return true;
+}
+
+fn tsjson_struct(comptime T: type, comptime struct_type: TypeInfo.Struct, value: json.Value) !T {
+    std.debug.warn("Got here\n");
+    switch (value) {
+        .Object => |o| {
+            var ret = T{};
+            inline for (struct_type.fields) |field| {
+                if (o.getValue(field.name)) |val| {
+                    @field(ret, field.name) = try tsjson(field.field_type, val);
+                } else {
+                    return error.JSONMissingObjectField;
+                }
+            }
+            return ret;
+        },
+        else => return error.JSONExpectedObject,
+    }
 }
 
 fn tsjson(comptime T: type, value: json.Value) !T {
+    std.debug.warn("Actually running tsjson\n");
     const type_info = @typeInfo(T);
     const ret = try switch (type_info) {
         .Bool => switch (value) {
             .Bool => |b| b,
             else => error.JSONExpectedBool,
         },
-        .Struct => |struct_type| switch (value) {
-            .Object => |o| blk: {
-                var ret: T = undefined;
-                inline for (struct_type.fields) |field| {
-                    if (o.get(field.name)) |val| {
-                        @field(ret, field.name) = try tsjson(field.field_type, val.value);
-                    } else {
-                        break :blk error.JSONMissingObjectField;
-                    }
+        .Int => switch (value) {
+            .Integer => |i| blk: {
+                if (!i64InIntegerRange(T, i)) {
+                    break :blk error.JSONIntegerOutOfRange;
+                } else {
+                    break :blk @intCast(T, i);
                 }
-                break :blk ret;
             },
-            else => error.JSONExpectedObject,
+            else => error.JSONExpectedInt,
         },
-
+        .Struct => |struct_type| tsjson_struct(T, struct_type, value),
         else => error.JSONBadType,
     };
     return ret;
 }
 
 const TestStruct = struct {
+    //foo: bool = false,
+    bar: i64 = 0,
+    baz: i64 = 12,
+};
+
+test "basic add functionality" {
+    std.debug.warn("Actually running test\n");
+    var p = json.Parser.init(std.debug.global_allocator, false);
+    defer p.deinit();
+
+    const s = "{\"foo\": true, \"barz\": 1}";
+
+    var tree = try p.parse(s);
+    defer tree.deinit();
+
+    //var result = try tsjson(TestStruct, tree);
+    var result = try tsjson(TestStruct, tree.root);
+    std.debug.warn("\n{}\n", result);
+}
+
+const TestStruct2 = struct {
     image: Image,
 };
 
@@ -53,19 +91,3 @@ const Thumbnail = struct {
     height: i32,
     width: i32,
 };
-
-test "basic add functionality" {
-    var p = json.Parser.init(std.debug.global_allocator, false);
-    defer p.deinit();
-
-    const s = "{\"foo\": 32}";
-
-    var tree = try p.parse(s);
-    defer tree.deinit();
-
-    //var result = try tsjson(TestStruct, tree);
-    var result = try tsjson(struct {
-        foo: bool,
-    }, tree.root);
-    std.debug.warn("\n{}\n", result);
-}
